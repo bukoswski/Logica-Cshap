09a946e6bbf44ce26c830892742f3c2c983e7497
Pedro Mendes <mendespedrinho76@gmail.com>
2024-07-30 13:47:27 -0300

    Arrays

---
+++ Array.2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+//estrutura do array
+class Estrutura
+{
+    //metodo do que ira criar o array
+    public static void dados (int [] array, out int primeiro, out int segundo, out int terceiro)
+    {
+        // igualando todos com 0
+        primeiro = segundo = terceiro = 0;
+        //iteração com os elementos
+        foreach (int numero in array)
+        {
+            //logica aplicada na solucao
+            if(numero > primeiro)
+            {
+                terceiro = segundo;
+                segundo = primeiro;
+                primeiro = numero;
+            }
+            else if (numero > segundo)
+            {
+                terceiro = segundo; 
+                segundo = numero;
+            }
+            else if (numero > terceiro)
+            {
+                terceiro = numero;
+            }
+        }
+    }
+
+
+    // metodo main para rodar o codigo
+    public static void Main()
+    {   // insercao dos numeros no array
+        int [] array = { 6, 8, 1, 9, 2, 1, 10, 10};
+        //chamando o metodo estatico
+        Estrutura.dados (array, out int primeiro, out int segundo, out int terceiro);
+        // monstrando no terminal quais sao os maiores elementos do array
+        Console.WriteLine("Os três maiores números no array são: " + primeiro + ", " + segundo + ", " + terceiro);
+    }
+
+}
+
---
+++ Array/Program.cs
@@ -0,0 +1,31 @@
+﻿
+
+using System;
+
+class Array {
+    public static int search(int[] arr, int L, int x)
+    {
+        for (int i = 0; i < L; i++)
+        {
+            if (arr[i] == x)
+                return i;
+        }
+        return -1;
+    }
+
+    // Driver's code
+    public static void Main()
+    {
+        int[] arr = { 2, 3, 4, 10, 40 };
+        int x = 4;
+
+        // Function call
+        int result = search(arr, arr.Length, x);
+        if (result == -1)
+            Console.WriteLine(
+                "Element is not present in array");
+        else
+            Console.WriteLine("Element is present at index "
+                              + result);
+    }
+}
